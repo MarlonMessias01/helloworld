24b5a50bfc91e90df23140d4ae5eccf08e2b94df
MarlonMessias01 <marlonmessias02@gmail.com>
2024-03-12 21:31:05 -0300

    Login

--- _footer.php
+++ _footer.php
@@ -1,27 +1,31 @@
 </main>
+    
+    <footer>
 
-<footer>
+        <a href="index.php">
+            <i class="fa-solid fa-house fa-fw"></i>
+        </a>
+        <div>
+            <div>&copy; 2024 Eu Mesmo!</div>
+            <a href="privacy.php">Políticas de Privacidade</a>
+        </div>
+        <a href="#wrap">
+            <i class="fa-solid fa-circle-up fa-fw"></i>
+        </a>
 
-    <a href="index.php">
-        <i class="fa-solid fa-house fa-fw"></i>
-    </a>
-    <div>
-        <div>&copy; 2024 Eu Mesmo!</div>
-        <a href="privacy.php">Políticas de Privacidade</a>
+    </footer>
+    &nbsp;
     </div>
-    <a href="#wrap">
-        <i class="fa-solid fa-circle-up fa-fw"></i>
-    </a>
 
-</footer>
+    <?php // Importa as bibliotecas e configuração do Firebase ?>
+    <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-auth-compat.js"></script>
+    
+    <?php // Importa o JavaScript do tema ?>
+    <script src="assets/js/global.js"></script>
 
-</div>
+    <?php // Importa o JavaScript específico desta página dinâmicamente ?>
+    <script src="assets/js/<?php echo $page["js"] ?>"></script>
+    </body>
 
-<!-- Importa o JavaScript do tema -->
-<script src="assets/js/global.js"></script>
-
-<!-- Importa o JavaScript específico desta página -->
-<script src="assets/js/<?php echo $page["js"] ?>"></script>
-</body>
-
-</html>
+    </html>
--- assets/js/global.js
+++ assets/js/global.js
@@ -1,1 +1,66 @@
+//Chave firebase
 
+const firebaseConfig = { 
+    apiKey : "AIzaSyBZuHCZWVEQc1uQPICEr8GWF0dpRMFRjrA" , 
+    authDomain : "blog-helloword-2eb56.firebaseapp.com" , 
+    projectId : "blog-helloword-2eb56" , 
+    storageBucket : "blog-helloword-2eb56.appspot.com" , 
+    mensagensSenderId : "1004160470872" , 
+    appId : "1:1004160470872:web:f11af59155c1afacfb9f2d" 
+  };  
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Inicializa o Firebase Authentication
+const auth = firebase.auth();
+
+// Identifica elementos do HTML para interação
+const userAccess = document.getElementById('userAccess');
+const userImg = document.getElementById('userImg');
+const userIcon = document.getElementById('userIcon');
+const userLabel = document.getElementById('userLabel');
+
+// Monitora se houve mudanças na autenticação do usuário
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // Se alguém se logou, faça isso...
+        // Chama a função que trata o usuário logado
+        isLogged(user);
+    } else {
+        // Se alguém deslogou, faça isso...
+        // Chama a função que trata o usuário NÃO logado
+        notLogged();
+    }
+});
+
+// Função que trata o usuário logado
+function isLogged(user) {
+    // Altera href do link
+    userAccess.href = `profile.php`;
+    // Altera title do link
+    userAccess.title = `Ver perfil de ${user.displayName}`;
+    // Oculta o ícone de login
+    userIcon.style.display = 'none';
+    // Cria os atributos da imagem conforme dados do usuário
+    userImg.src = user.photoURL;
+    userImg.alt = user.displayName;
+    // Mostrar a imagem do usuário
+    userImg.style.display = 'inline';
+    // Altera a label para entrar
+    userLabel.innerHTML = 'Perfil';
+}
+
+// Função que trata o usuário NÃO logado 
+function notLogged() {
+    // Altera href do link
+    userAccess.href = `login.php?ref=${location.href}`;
+    // Altera title do link
+    userAccess.title = 'Logue-se';
+    // Oculta a imagem do usuário
+    userImg.style.display = 'none';
+    // Mostra o ícone de login
+    userIcon.style.display = 'inline';
+    // Altera a label para entrar
+    userLabel.innerHTML = 'Entrar';
+}
